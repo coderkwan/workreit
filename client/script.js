bff3aaf0299523187a52dace73071205cf543885
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2025-07-11 21:38:22 +0200

    database storing basic

--- client/script.js
+++ client/script.js
@@ -5,18 +5,46 @@ const button = document.getElementById("button")
 let running = false
 let main_interval
 
-form.addEventListener("submit", (e) => {
+async function getData() {
+    const res = await fetch("http://localhost:3001/", {method: "GET"})
+    if (res.ok) {
+        const body = await res.json()
+        const items = body.data.reverse()
+        items.forEach((item, index) => {
+            data_container.appendChild(createTaskElement(item))
+        })
+    } else {
+        console.log("cant find content")
+    }
+}
+getData()
+
+form.addEventListener("submit", (e) => createTask(e))
+
+
+async function createTask(e) {
     e.preventDefault()
+    task_input.disabled = true
 
     const name = e.target.task.value
     const start_time = Date.now()
+    const data = {task: name, start_time, tag: "", project: ""}
 
     if (!running) {
-        UpdateTime(start_time, name)
+
+        const res = await fetch("http://localhost:3001/create", {method: "POST", body: JSON.stringify(data), headers: {"Content-Type": "application/json"}})
+
+        if (res.ok) {
+            const done = await res.json()
+            // Store and use ID
+            UpdateTime(start_time, name)
+        } else {
+            console.log("Failed")
+        }
     } else {
         stopTimer()
     }
-})
+}
 
 function UpdateTime(start_time) {
     running = true
@@ -28,9 +56,62 @@ function UpdateTime(start_time) {
 }
 
 function stopTimer() {
+    task_input.disabled = false
     running = false
     button.innerText = "Start"
     display.innerText = "00:00:00"
     form.reset()
     clearInterval(main_interval)
+}
+
+
+
+function createTaskElement(data) {
+    const task = document.createElement('div')
+    const task_details = document.createElement('div')
+    const task_title = document.createElement('div')
+    const task_time = document.createElement('div')
+    const task_options = document.createElement('div')
+
+    const title = document.createElement('h3')
+    const project = document.createElement('p')
+    const tag = document.createElement('p')
+    const start = document.createElement('p')
+    const end = document.createElement('p')
+    const continue_btn = document.createElement('button')
+    const options_btn = document.createElement('button')
+
+    title.innerText = data.task;
+    project.innerText = data.project
+    tag.innerText = data.tag
+    start.innerText = data.start_time
+    end.innerText = data.end_time
+    continue_btn.innerText = 'Cont'
+    options_btn.innerText = '...'
+
+    task_title.appendChild(title)
+    task_title.appendChild(project)
+
+    task_details.appendChild(tag)
+
+    task_time.appendChild(start)
+    task_time.appendChild(end)
+
+    task_options.appendChild(continue_btn)
+    task_options.appendChild(options_btn)
+
+    task_details.appendChild(task_time)
+    task_details.appendChild(task_options)
+
+    task.appendChild(task_title)
+    task.appendChild(task_details)
+
+
+    task.className = 'task'
+    task_time.className = 'task__time'
+    task_details.className = 'task__details'
+    task_options.className = 'task__options'
+    task_title.className = 'task__title'
+
+    return task
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import "dotenv/config"
 import express from 'express'
 import {turso} from './db.js'
+import cors from 'cors'
 
 const _PORT = 3001
 const server = express()
+server.use(cors())
+server.use(express.json())
 
 
 async function getTasks() {
@@ -12,12 +15,33 @@ async function getTasks() {
     );
 
     const rows = data.rows
-    console.log(rows)
-
     return rows
 }
-getTasks()
 
+server.get("/", async (req, res) => {
+    const data = await getTasks()
+    res.send({data})
+})
+
+server.post("/create", async (req, res) => {
+    const data = req.body
+
+    try {
+        const query = await turso.execute(
+            `INSERT INTO tracker (task, start_time) VALUES ('${data.task}', '${data.start_time}');`
+        );
+        console.log(query)
+        if (query.rowsAffected == 1) {
+
+            res.status(200).send({message: "Task created Succesfully!", id: Number(query.lastInsertRowid)})
+        } else {
+            throw new Error("Failed to create a task!")
+        }
+
+    } catch (e) {
+        res.status(401).send({message: e.message})
+    }
+})
 
 
 server.listen(_PORT, () => console.log(`Server running at port ${_PORT}`))
